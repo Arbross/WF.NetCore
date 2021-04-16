0fe3d5ec5daae97154136fe5dbd891f9d76dbeac
Victor <victor@my.home>
2021-04-16 15:59:13 +0300

    First lesson. WF

---
+++ Windows Forms First Lesson/Windows Forms First Lesson/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Windows_Forms_First_Lesson
+{
+    partial class MoinCruft
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MoinCruft));
+            this.Start = new System.Windows.Forms.Button();
+            this.lblClick = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Start.BackgroundImage")));
+            this.Start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Start.Location = new System.Drawing.Point(437, 221);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(100, 100);
+            this.Start.TabIndex = 0;
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Visible = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // lblClick
+            // 
+            this.lblClick.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblClick.Location = new System.Drawing.Point(356, 9);
+            this.lblClick.Name = "lblClick";
+            this.lblClick.Size = new System.Drawing.Size(181, 78);
+            this.lblClick.TabIndex = 1;
+            this.lblClick.Text = "Click me";
+            this.lblClick.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblClick.Click += new System.EventHandler(this.lblClick_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblInfo.Location = new System.Drawing.Point(356, 107);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(181, 78);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "Info";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MoinCruft
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(549, 330);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblClick);
+            this.Controls.Add(this.Start);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MoinCruft";
+            this.Text = "MineCruft";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label lblClick;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ Windows Forms First Lesson/Windows Forms First Lesson/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Forms_First_Lesson
+{
+    public partial class MoinCruft : Form
+    {
+        public MoinCruft()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Maximized;
+            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+
+            while (true)
+            {
+                DialogResult dr = MessageBox.Show("Would you like to color form?", "Event click", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Question);
+                MessageBox.Show($"You choose : {DialogResult}", "Info", MessageBoxButtons.OK);
+                if (dr == DialogResult.Abort)
+                {
+                    // this.BackColor = Color.LightBlue;
+                    Random rnd = new Random();
+                    Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                    BackColor = randomColor;
+                    break;
+                }
+            }
+            Start.Visible = true;
+            lblInfo.Update();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Full screen
+            //this.TopMost = true;
+            //this.FormBorderStyle = FormBorderStyle.None;
+            //this.WindowState = FormWindowState.Maximized;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("Form close", "Event FormClosed");
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are u sure?", "Closing form", MessageBoxButtons.YesNo, MessageBoxIcon.Question) != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+        }
+        int counter = 0;
+        private void lblClick_Click(object sender, EventArgs e)
+        {
+            lblInfo.Text = $"Was clicked : {++counter}";
+        }
+    }
+}
